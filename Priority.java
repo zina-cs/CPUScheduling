76e5a9a91c7ff7f53954694ea0336cba4040a518
ZA8422 <109593976+ZA8422@users.noreply.github.com>
2022-09-02 12:14:52 +0400

    Create Priority.java

---
+++ Priority.java
@@ -0,0 +1,123 @@
+package packag; 
+
+import java.util.ArrayList; 
+
+import java.util.Collections; 
+
+  
+
+  
+
+public class Priority implements Algorithm { 
+
+ 
+
+ArrayList<Task> queue; 
+
+  
+
+public Priority () {} 
+
+public Priority (ArrayList<Task> q) 
+
+{ 
+
+//We will arrange q into queue by Priority of Tasks 
+
+for (int i = 0; i < q.size(); i++) 
+
+{ 
+
+for (int j = i+1; j< q.size(); j++) 
+
+{  
+
+Task x = q.get(i); 
+
+Task y = q.get(j); 
+
+if (x.getPriority()> y.getPriority())    
+
+{   
+
+Collections.swap(q, i, j);  
+
+}   
+
+}} 
+
+queue  = new ArrayList<Task>(q); 
+
+} 
+
+ 
+
+@Override 
+
+public void schedule() { 
+
+  
+
+int sumturnaround = 0; 
+
+for (int i = queue.size(), j = 0; i>0 && j < queue.size(); i--, j++) 
+
+{ 
+
+sumturnaround += i*(queue.get(j).getBurst()); 
+
+} 
+
+int avgturnaround = sumturnaround/queue.size(); 
+
+ 
+
+int sumwaiting = 0; 
+
+for (int i = queue.size() - 1,j = 0; i > 0 && j < queue.size(); i--, j++) 
+
+{ 
+
+sumwaiting += i*(queue.get(j).getBurst()); 
+
+} 
+
+int avgwaiting = sumwaiting/queue.size(); 
+
+ 
+
+while (queue.isEmpty() != true) 
+
+{ 
+
+Task result = pickNextTask(); 
+
+CPU.run(result, result.getBurst()); 
+
+queue.remove(0); 
+
+} 
+
+System.out.println("Average Turnaround Time: "+ avgturnaround); 
+
+System.out.println("Average Waiting Time: "+ avgwaiting); 
+
+System.out.println("Average Response Time: "+ avgwaiting); 
+
+} 
+
+  
+
+@Override 
+
+public Task pickNextTask() { 
+
+Task choice = queue.get(0); 
+
+return choice; 
+
+} 
+
+  
+
+} 
